27ea1bbc988f2d1180e7ba6241ec6eae58584194
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:02:33 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//34
+int main(){
+	int a=0;
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )%c%c%c\\\n",34,34,34);
+	printf("|%c^%c`    |\n",34,34);
+	printf("||_/=\\\\__|\n");
+
+}
