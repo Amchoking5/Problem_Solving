b2de500820b18e7e47a1a2c9fac6b99d0ccbcbe2
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:41:15 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 716 ms, Memory: 76376 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(r):
+    global visited, nodes, count
+    visited[r] = count
+    count+=1
+    inode = nodes[r]
+    inode.sort()
+    for i in inode:
+        if(visited[i]==0):
+            dfs(i)
+    
+    
+n,m,r = map(int,input().split())
+visited = [0 for i in range(n+1)]
+nodes = {k: [] for k in range(n+1)}
+for i in range(m):
+    a,b = map(int,input().split())
+    nodes[a].append(b)
+    nodes[b].append(a)
+count =1
+dfs(r)
+
+for i in range(1,n+1):
+    print(visited[i])
