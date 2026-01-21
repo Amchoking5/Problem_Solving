ee5f26f0237f99626d120aec17d1d753a347d655
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:02:10 +0900

    [Bronze III] Title: 전자레인지, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n%10!=0){
+        printf("-1");
+        return 0;
+    }
+    printf("%d ",(n/300));
+    n=n%300;
+    printf("%d ",(n/60));
+    n=n%60;
+    printf("%d ",(n/10));
+}
