8d2aad83610c4995a603fe3707b4911f7fa76f45
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:15:24 +0900

    [Platinum IV] Title: 열혈강호 2, Time: 1196 ms, Memory: 5216 KB -BaekjoonHub

---
+++ 백준/Platinum/11376. 열혈강호 2/열혈강호 2.cc
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+int n,m,count=0;
+vector<int> choose[1005];
+bool check[1005];
+int cow[1005];
+
+bool bfs(int x){
+	for(int i=0;i<choose[x].size();i++){
+		int cage=choose[x][i];
+		if(check[cage]) continue;
+		check[cage]=true;
+		if(cow[cage]==0 || bfs(cow[cage])){
+			cow[cage]=x;
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(){
+	scanf("%d %d",&n,&m);
+	for(int i=1;i<=n;i++){
+		int k;
+		scanf("%d",&k);
+		for(int j=0;j<k;j++){
+			int c;
+			scanf("%d",&c);
+			choose[i].push_back(c);
+		}
+	}
+	for(int i=1;i<=n;i++){
+		fill(check,check+m+1,false);
+		if(bfs(i)) count++;
+		fill(check,check+m+1,false);
+		if(bfs(i)) count++;
+	}
+	printf("%d",count);
+}
