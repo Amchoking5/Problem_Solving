0bdefb30035b94235b346fb9fd2d0586d0113b82
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:40:55 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 2160 ms, Memory: 66136 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def bfs(r):
+    global visited,nodes
+    count=1
+    visited[r] = count
+    count+=1
+    nextgo=[r]
+    while(len(nextgo)!=0):
+        k=nextgo.pop(0)
+        listnode = nodes[k]
+        listnode.sort()
+        for j in range (len(listnode)):
+            if(visited[listnode[j]]==0):
+                visited[listnode[j]]=count
+                count+=1
+                nextgo.append(listnode[j])
+
+n,m,r = map(int,input().split())
+visited =[0 for i in range(n+1)]
+nodes = { k: [] for k in range(n+1)}
+for i in range (m):
+    a,b = map(int,input().split())
+    nodes[a].append(b)
+    nodes[b].append(a)
+
+bfs(r)
+for i in range(1,n+1):
+    print(visited[i])
