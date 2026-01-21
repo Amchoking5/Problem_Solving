2cf0986df93843ef242b66594f777b3889cd261c
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:09:33 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int fibo(int a){
+	if (a==0) return 0;
+	if (a==1 || a==2) return 1;
+	return fibo(a-1)+fibo(a-2);
+}
+
+int main (){
+	int a;
+	scanf("%d",&a);
+	printf("%d",fibo(a));
+}
