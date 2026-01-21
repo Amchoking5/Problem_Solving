06e1d17063ff490aa9c1937fb0277bc5a6f894c3
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:29:13 +0900

    [Silver III] Title: N과 M (1), Time: 36 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int n,m;
+
+void npr(int k,int check[9],int arr[9]){
+	if(m==k){
+		for(int i=0;i<k;i++){
+			printf("%d ",arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for(int i=1;i<n+1;i++){
+		if(check[i]==0){
+			check[i]=1;
+			arr[k]=i;
+			npr(k+1,check,arr);
+			check[i]=0;
+		}
+	}
+	
+}
+
+int main(){
+	int check[9]={0};
+	int arr[9]={0};
+	scanf("%d %d",&n,&m);
+	npr(0,check,arr);
+}
