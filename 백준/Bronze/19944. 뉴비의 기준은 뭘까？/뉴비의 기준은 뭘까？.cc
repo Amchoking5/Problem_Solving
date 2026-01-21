4f8115a4165f788dff05a9339ba913bebb403f11
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:38:10 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int n,m;
+	scanf("%d %d",&n,&m);
+	if(m==1 || m==2){
+		printf("NEWBIE!");
+	}
+	else if(n>=m){
+		printf("OLDBIE!");
+	}
+	else{
+		printf("TLE!");
+	}
+}
