4b936a46a21a105e6eb58b3f3c4f23bf2bd59acd
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 01:26:27 +0900

    [Silver IV] Title: 카드2, Time: 0 ms, Memory: 5020 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int q[1000001];
+int head=0,tail=0;
+
+int main(){
+	int n;
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+		q[tail]=tail+1;
+		tail++;
+	}
+	while(tail-head!=1){
+		head++;
+		q[tail]=q[head];
+		tail++;
+		head++;
+	}
+	printf("%d",q[head]);
+}
