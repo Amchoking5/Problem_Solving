79c74e8cb54d62ec5a3d3ce22461a90aeb473098
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:14:43 +0900

    [Bronze IV] Title: 한글 2, Time: 64 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.py
@@ -0,0 +1,16 @@
+korean1=["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
+korean2=["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"]
+korean3=[" ","ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
+
+def korean_to_be_englished(korean_word):
+    sum=1
+    for w in list(korean_word.strip()):
+        ch1 = (ord(w) - ord('가'))//588
+        ch2 = ((ord(w) - ord('가')) - (588*ch1)) // 28
+        ch3 = (ord(w) - ord('가')) - (588*ch1) - 28*ch2
+        r_lst=[ch1, ch2, ch3]
+    return r_lst
+
+
+check_list=korean_to_be_englished(input())
+print(int(check_list[0])*21*28 + int(check_list[1])*28 + int(check_list[2]) +1)
