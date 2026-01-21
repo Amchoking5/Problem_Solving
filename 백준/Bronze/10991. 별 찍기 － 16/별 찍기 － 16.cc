d384ef8fec9dfbb68c6bc95f24095628de5aa0ba
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:11:52 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	scanf("%d",&n);
+	for(int i=n-1;i>=0;i--){
+		for(int j=0;j<i;j++){
+			printf(" ");
+		}
+		for(int j=i;j<n;j++){
+			printf("* ");
+		}
+		printf("\n");
+	}
+}
