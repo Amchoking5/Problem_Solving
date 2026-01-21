47fcf2669ca02ed356c1fe30a665640bd6316bbc
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:20:35 +0900

    [Gold V] Title: CCW, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int x1,x2,x3,y1,y2,y3;
+	scanf("%d %d",&x1,&y1);
+	scanf("%d %d",&x2,&y2);
+	scanf("%d %d",&x3,&y3);
+	int s;
+	s=(x1*y2+x2*y3+x3*y1)-(x2*y1+x3*y2+x1*y3);
+	if(s<0) printf("-1");
+	else if(s==0) printf("0");
+	else printf("1");
+}
