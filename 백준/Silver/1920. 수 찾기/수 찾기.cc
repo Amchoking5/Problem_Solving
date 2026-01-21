07769ffc2b551ee47aa492117e441b204b95803e
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 01:21:25 +0900

    [Silver IV] Title: 수 찾기, Time: 100 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int location=0;
+int a[100001]={0};
+int b[100001]={0};
+
+int compare (const void* first, const void* second)
+{
+    if (*(int*)first > *(int*)second)
+        return 1;
+    else if (*(int*)first < *(int*)second)
+        return -1;
+    else
+        return 0;
+}
+
+int main(){
+	bool findx=false;
+	int n,m,left,right,mid;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	scanf("%d",&m);
+	for(int i=0;i<m;i++){
+		scanf("%d",&b[i]);
+	}
+	
+	qsort(a, n, sizeof(int), compare);
+	
+	for(int i=0;i<m;i++){
+		left=0,right=n-1;
+		findx=false;
+		while(left<=right){
+			mid=(left+right)/2;
+			if(a[mid]>b[i])
+				right = mid-1;
+			else if(a[mid]<b[i])
+				left=mid+1;
+			else if(a[mid]==b[i]){
+				printf("1\n");
+				findx=true;
+				break;
+			}
+		}
+		if(!findx) printf("0\n");
+	}
+}
