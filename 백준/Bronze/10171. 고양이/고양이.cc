2d4f8544f7a2bdafdbf076af98a7d77da46376b8
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:02:21 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// / 47
+// \ 92 
+//   32
+// ( 40
+// ) 41
+int main(){
+	int a=0;
+	printf("\\    %c\\\n",47);
+	printf(" %c  %c %c%c\n",41,40,39,41);
+	printf("%c  %c  %c\n",40,47,41);
+	printf(" \\%c%c%c%c%c\n",40,95,95,41,124);
+}
