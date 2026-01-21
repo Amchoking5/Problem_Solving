676d839539e9c74b307189b067711be6c2b5b32a
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:17:43 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 64 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+struct Where{
+	int x;
+	int y;
+};
+
+bool compare(const Where &a , const Where &b){
+	if(a.x==b.x){
+		return a.y<b.y;
+	}
+	return a.x<b.x;
+}
+int main(){
+	int n;
+	scanf("%d",&n);
+	
+	Where *where = new Where[n];
+	
+	for(int i=0;i<n;i++){
+		scanf("%d %d",&where[i].y,&where[i].x);
+	}
+	
+    sort(where , where+n , compare);
+    
+    for(int i=0;i<n;i++){
+		printf("%d %d\n",where[i].y,where[i].x);		
+	}
+}
