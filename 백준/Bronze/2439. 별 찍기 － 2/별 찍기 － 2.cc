6338e8d9832d77cbad7b16332d83ad76d3e2097e
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 01:30:08 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 944 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+    {
+    int a,i,j,k;
+    scanf("%d",&a);
+    for(i=0;i<a;i++){
+        for(j=0;j<a-i-1;j++){
+            printf(" ");
+        }
+        for(k=0;k<i+1;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
