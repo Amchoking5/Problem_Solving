cffef35ee5048cab367ef0ae8070f92e03f575c2
Amchoking5 <amchoking@kaist.ac.kr>
2026-01-22 02:28:52 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 328 ms, Memory: 4900 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int t,a,b,ar[1000000];
+	scanf("%d",&t);
+	for(int i=0;i<t;i++){
+		scanf("%d %d",&a,&b);
+		ar[i]=a+b;
+	}
+	for(int i=0;i<t;i++){
+		printf("%d\n",ar[i]);
+	}
+}
